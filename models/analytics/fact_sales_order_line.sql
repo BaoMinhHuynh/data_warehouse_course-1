0c1677648c7db652953071ac1e0b56cb0d7f791a
BaoMinhHuynh <huynhbaominh.h1.14cla@gmail.com>
2024-12-24 13:46:55 +0700

    join voi bang header

--- models/analytics/fact_sales_order_line.sql
+++ models/analytics/fact_sales_order_line.sql
@@ -24,10 +24,14 @@ WITH fact_sales_order_line__source AS (
 )
 
 SELECT 
-  sale_order_line_key,
-  sale_order_key,
-  product_key,
-  quantity,
-  unit_price,
-  gross_amount
-FROM fact_sales_order_line__calculate_measure 
+  fact_line.sale_order_line_key,
+  fact_line.sale_order_key,
+  fact_header.customer_key,
+  fact_line.product_key,
+  fact_line.quantity,
+  fact_line.unit_price,
+  fact_line.gross_amount
+FROM fact_sales_order_line__calculate_measure AS fact_line
+LEFT JOIN `weighty-flag-435812-j2.wide_world_importers_dwh_staging.stg_fact_sales_order` AS fact_header
+ON fact_line.sale_order_key = fact_header.sale_order_key
+
